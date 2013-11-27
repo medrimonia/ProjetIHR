86254cc7bc437717bd20bcc3fedab0174470f813
David <dbitonneau@enseirb-matmeca.fr>
2013-11-27 23:04:03 +0100

    Improved formula + CamelCase correction

--- comportement/ExperienceDatabase.py
+++ comportement/ExperienceDatabase.py
@@ -6,8 +6,8 @@ import math
 stimuliList = ["Mouchoir", "Mouton", "Caresse", "Tape"]
 feelingList = ["+", "-", "Nothing"]
 
-default_tau = 5
-default_alpha = 0.5
+defaultTau = 5
+defaultAlpha = 0.5
 
 stimuliValues = {}
 for s in stimuliList:
@@ -24,19 +24,25 @@ def fearThreshold():
 def excitationThreshold():
     return 1.5 * 1.0 / len(feelingList)
 
+def customScoreFormula(dt, tau, alpha):
+    return math.e**(- (dt / tau)**alpha)
+
 class Experience:
 
     # Initialize an experience with a stimuli perceived and an
     # associated feeling
     # perception must be a member of the stimuli list
     # feeling must be a member of the feelingList
-    def __init__(self, perception, feeling, time_s):
+    def __init__(self, perception, feeling, time):
         self.p = perception
         self.f = feeling
-        self.t = time_s
+        self.t = time
 
     def score(self, t, tau, alpha):
-        return math.e**(- ((t - self.t) / tau)**alpha)
+        if t >= self.t:
+            return customScoreFormula(t - self.t, tau, alpha)
+        else:
+            return 0
 
 
 # This class can store various experiences concerning a stimuli.
@@ -48,23 +54,23 @@ class StimuliExperiences:
         for f in feelingList:
             self.experiences[f] = []
 
-    def scoreExperiences(self, time_s, tau, alpha):
+    def scoreExperiences(self, time, tau, alpha):
         n = 0
         for key, val in self.experiences.items():
-            n += sum([e.score(time_s, tau, alpha) for e in val])
+            n += sum([e.score(time, tau, alpha) for e in val])
         return n
 
-    def scoreExperiencesWithFeeling(self, feeling, time_s, tau, alpha):
-        return sum([e.score(time_s, tau, alpha) for e in self.experiences[feeling]])
+    def scoreExperiencesWithFeeling(self, feeling, time, tau, alpha):
+        return sum([e.score(time, tau, alpha) for e in self.experiences[feeling]])
  
     # experience must be a stimuli of the Experience class
     def addExperience(self, experience):
         feeling = experience.f
         self.experiences[feeling] += [experience]
 
-    def smoothedProbability(self, feeling, k, time_s, tau, alpha):
-        numerator = self.scoreExperiencesWithFeeling(feeling, time_s, tau, alpha) + k
-        denominator = self.scoreExperiences(time_s, tau, alpha) + k * len(feelingList)
+    def smoothedProbability(self, feeling, k, time, tau, alpha):
+        numerator = self.scoreExperiencesWithFeeling(feeling, time, tau, alpha) + k
+        denominator = self.scoreExperiences(time, tau, alpha) + k * len(feelingList)
         return numerator / denominator
 
 # Storing
@@ -78,24 +84,24 @@ class ExperienceDatabase:
     def addExperience(self, experience):
         self.experiences[experience.p].addExperience(experience)
 
-    def scoreExperiences(self, time_s, tau, alpha):
+    def scoreExperiences(self, time, tau, alpha):
         n = 0
         for key, val in self.experiences.items():
-            n += val.scoreExperiences(time_s, tau, alpha)
+            n += val.scoreExperiences(time, tau, alpha)
         return n
 
-    def scoreExperiencesWithStimuli(self, s, time_s, tau, alpha):
-        return self.experiences[s].scoreExperiences(time_s, tau, alpha)
+    def scoreExperiencesWithStimuli(self, s, time, tau, alpha):
+        return self.experiences[s].scoreExperiences(time, tau, alpha)
 
-    def smoothedStimuliProbability(self, s, k, time_s, tau, alpha):
-        numerator = self.scoreExperiencesWithStimuli(s, time_s, tau, alpha) + k
-        denominator = self.scoreExperiences(time_s, tau, alpha) + k * len(stimuliList)
+    def smoothedStimuliProbability(self, s, k, time, tau, alpha):
+        numerator = self.scoreExperiencesWithStimuli(s, time, tau, alpha) + k
+        denominator = self.scoreExperiences(time, tau, alpha) + k * len(stimuliList)
         return numerator / denominator
 
     # return laplacian smoothed probability of P(f|o)
     # s : stimuli
-    def smoothedFeelingProbability(self, s, feeling, k, time_s, tau, alpha):
-        return self.experiences[s].smoothedProbability(feeling, k, time_s, tau, alpha)
+    def smoothedFeelingProbability(self, s, feeling, k, time, tau, alpha):
+        return self.experiences[s].smoothedProbability(feeling, k, time, tau, alpha)
         
 
 class PredictiveModel:
@@ -104,31 +110,34 @@ class PredictiveModel:
         self.lastStimuli = None
         self.k = k
 
-    def addStimuli(self, time_s, newStimuli):
+    def addStimuli(self, time, newStimuli):
         if (self.lastStimuli != None):
-            e = Experience(self.lastStimuli, stimuliValues[newStimuli], time_s)
+            e = Experience(self.lastStimuli, stimuliValues[newStimuli], time)
             self.experiences.addExperience(e)
         self.lastStimuli = newStimuli
 
-    def feelingProbability(self, feeling, time_s, tau, alpha):
-        return self.experiences.smoothedFeelingProbability(self.lastStimuli, feeling, self.k, time_s, tau, alpha);
+    def feelingProbability(self, feeling, time, tau, alpha):
+        return self.experiences.smoothedFeelingProbability(self.lastStimuli, feeling, self.k, time, tau, alpha);
 
-    def reaction(self, time_s, tau, alpha):
+    def reaction(self, time, tau, alpha):
         if self.lastStimuli == None:
             return "nothing"
 
-        pLastStimuli = self.experiences.smoothedStimuliProbability(self.lastStimuli, self.k, time_s, tau, alpha)
-        print("Last Stimuli "+str(pLastStimuli))
+        pLastStimuli = self.experiences.smoothedStimuliProbability(self.lastStimuli, self.k, time, tau, alpha)
+        pSomethingBad = self.feelingProbability("-", time, tau, alpha)
+        pSomethingGood = self.feelingProbability("+", time, tau, alpha)
+
+        #print("Last Stimuli "+str(pLastStimuli))
+        #print("Something Bad "+str(pSomethingBad))
+        #print("Something good "+str(pSomethingGood))
+        print(time, pSomethingBad, pSomethingGood, 1-pSomethingBad-pSomethingGood)
+
         if (pLastStimuli < surpriseThreshold()):
             return "surprise"
 
-        pSomethingBad = self.feelingProbability("-", time_s, tau, alpha)
-        print("Something Bad "+str(pSomethingBad))
         if (pSomethingBad > fearThreshold()):
             return "fear"
 
-        pSomethingGood = self.feelingProbability("+", time_s, tau, alpha)
-        print("Something good "+str(pSomethingGood))
         if (pSomethingGood > excitationThreshold()):
             return "excitation"
 
@@ -136,7 +145,24 @@ class PredictiveModel:
 
 
 if __name__ == "__main__":
-    model = PredictiveModel(1)
+    c = 80
+    k = math.e**(- (c / defaultTau)**defaultAlpha)
+    model = PredictiveModel(k)
+    model.addStimuli( 1, "Mouton")
+    model.addStimuli( 1, "Caresse")
+    model.addStimuli( 2, "Mouton")
+    model.addStimuli( 2, "Caresse")
+    model.addStimuli( 3, "Mouton")
+    model.addStimuli( 3, "Tape")
+    model.addStimuli( 4, "Mouton")
+    timeToWatch = 100
+    for dixT in range(0, 10 * timeToWatch, 1):
+        t = dixT / 10
+        model.reaction(t, defaultTau, defaultAlpha)
+
+
+def test1():
+    model = PredictiveModel(0.02)
     model.addStimuli( 0, "Mouton")
     model.addStimuli( 1, "Caresse")
     model.addStimuli( 2, "Mouton")
@@ -154,6 +180,5 @@ if __name__ == "__main__":
     model.addStimuli(14, "Mouchoir")
     model.addStimuli(15, "Tape")
     model.addStimuli(16, "Mouchoir")
-    print(model.reaction(17, default_tau, default_alpha))
-    print(model.reaction(20, default_tau, default_alpha))
-    
+    print(model.reaction(17, defaultTau, defaultAlpha))
+    print(model.reaction(20, defaultTau, defaultAlpha))
