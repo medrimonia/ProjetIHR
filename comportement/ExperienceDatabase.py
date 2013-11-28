f2a6073dfef2d8342a22876f3e3035e0dc11d941
David <dbitonneau@enseirb-matmeca.fr>
2013-11-28 23:33:41 +0100

    Separate file for database creation and result display + added command line argument

--- comportement/ExperienceDatabase.py
+++ comportement/ExperienceDatabase.py
@@ -2,14 +2,9 @@
 
 import math
 
-printHistory = False
-
 #TODO stimuliList and feeling list might be filled dynamically too
 stimuliList = ["Mouchoir", "Mouton", "Caresse", "Tape"]
 feelingList = ["+", "-", "Nothing"]
-
-defaultTau = 5
-defaultAlpha = 0.5
 
 stimuliValues = {}
 for s in stimuliList:
@@ -75,7 +70,6 @@ class StimuliExperiences:
         denominator = self.scoreExperiences(time, tau, alpha) + k * len(feelingList)
         return numerator / denominator
 
-# Storing
 class ExperienceDatabase:
 
     def __init__(self):
@@ -107,11 +101,15 @@ class ExperienceDatabase:
         
 
 class PredictiveModel:
-    def __init__(self, k):
+    def __init__(self, k, smartHistory = False):
+        ''' If smartHistory is set to True, feelingProbability will be
+        computed for the stimuli that occured just before the given time.
+        Otherwise feelingProbability is computed for the last stimuli added.'''
         self.experiences = ExperienceDatabase()
         self.lastStimuli = None
         self.stimuliTime = {}
         self.k = k
+        self.smartHistory = smartHistory
 
     def addStimuli(self, time, newStimuli):
         if (self.lastStimuli != None):
@@ -128,23 +126,22 @@ class PredictiveModel:
             return 0
         stimuliAtTime = stimuliBeforeTime[-1]
         lastStimuliAtTime = stimuliAtTime[-1]
-        if printHistory:
+        if self.smartHistory:
             return self.experiences.smoothedFeelingProbability(lastStimuliAtTime, feeling, self.k, time, tau, alpha);
         else:
             return self.experiences.smoothedFeelingProbability(self.lastStimuli, feeling, self.k, time, tau, alpha);
+
+    def reactionProbaRepartition(self, time, tau, alpha):
+        pLastStimuli = self.experiences.smoothedStimuliProbability(self.lastStimuli, self.k, time, tau, alpha)
+        pSomethingBad = self.feelingProbability("-", time, tau, alpha)
+        pSomethingGood = self.feelingProbability("+", time, tau, alpha)
+        return pLastStimuli, pSomethingBad, pSomethingGood
 
     def reaction(self, time, tau, alpha):
         if self.lastStimuli == None:
             return "nothing"
 
-        pLastStimuli = self.experiences.smoothedStimuliProbability(self.lastStimuli, self.k, time, tau, alpha)
-        pSomethingBad = self.feelingProbability("-", time, tau, alpha)
-        pSomethingGood = self.feelingProbability("+", time, tau, alpha)
-
-        #print("Last Stimuli "+str(pLastStimuli))
-        #print("Something Bad "+str(pSomethingBad))
-        #print("Something good "+str(pSomethingGood))
-        print(time, pSomethingBad, pSomethingGood, 1-pSomethingBad-pSomethingGood)
+        pLastStimuli, pSomethingBad, pSomethingGood = self.reactionProbaRepartition(time, tau, alpha)
 
         if (pLastStimuli < surpriseThreshold()):
             return "surprise"
@@ -156,43 +153,3 @@ class PredictiveModel:
             return "excitation"
 
         return "nothing"
-
-
-if __name__ == "__main__":
-    c = 80
-    k = math.e**(- (c / defaultTau)**defaultAlpha)
-    model = PredictiveModel(k)
-    model.addStimuli( 1, "Mouton")
-    model.addStimuli( 2, "Caresse")
-    model.addStimuli( 5, "Mouton")
-    model.addStimuli( 6, "Caresse")
-    model.addStimuli( 10, "Mouton")
-    model.addStimuli( 11, "Tape")
-    model.addStimuli( 15, "Mouton")
-    timeToWatch = 100
-    for dixT in range(0, 10 * timeToWatch, 1):
-        t = dixT / 10
-        model.reaction(t, defaultTau, defaultAlpha)
-
-
-def test1():
-    model = PredictiveModel(0.02)
-    model.addStimuli( 0, "Mouton")
-    model.addStimuli( 1, "Caresse")
-    model.addStimuli( 2, "Mouton")
-    model.addStimuli( 3, "Caresse")
-    model.addStimuli( 4, "Mouton")
-    model.addStimuli( 5, "Tape")
-    model.addStimuli( 6, "Mouton")
-    model.addStimuli( 7, "Caresse")
-    model.addStimuli( 8, "Mouton")
-    model.addStimuli( 9, "Caresse")
-    model.addStimuli(10, "Mouton")
-    model.addStimuli(11, "Tape")
-    model.addStimuli(12, "Mouchoir")
-    model.addStimuli(13, "Tape")
-    model.addStimuli(14, "Mouchoir")
-    model.addStimuli(15, "Tape")
-    model.addStimuli(16, "Mouchoir")
-    print(model.reaction(17, defaultTau, defaultAlpha))
-    print(model.reaction(20, defaultTau, defaultAlpha))
